833ed294f4a11b0c20efd7bbf4098f04572b8582
Kiryu <13matheusilva@gmail.com>
2024-03-21 11:36:34 -0300

    success div add

--- src/components/signUp/signUp.js
+++ src/components/signUp/signUp.js
@@ -1,6 +1,7 @@
 import React from "react";
 import desktopImg from "../../images/illustration-sign-up-desktop.svg"
 import mobileImg from "../../images/illustration-sign-up-mobile.svg"
+import success from "../../images/icon-success.svg"
 import ListItem from "../listItem/listItem";
 import validation from "../validation/validation";
 
@@ -8,30 +9,39 @@ import './signUp.css'
 
 const SignUp = () => {
     return (
-        <div className="signUp">
-            <div className="info">
-                <h2 className="title">Stay updated!</h2>
-                <p className="description">Join 60,000+ product managers receiving monthly updates on:</p>
-                <ul>
-                    <ListItem text='Product discovery and building what matters'/>
-                    <ListItem text='Measuring to ensure updates are a success'/>
-                    <ListItem text='And much more!'/>
-                </ul>
-                <div className="inputs">
-                    <div className="messages">
-                        <p className="email">Email address</p>
-                        <p className="error none">Valid email required</p>
-                    </div>
+        <>
+            <div className="signUp">
+                <div className="info">
+                    <h2 className="title">Stay updated!</h2>
+                    <p className="description">Join 60,000+ product managers receiving monthly updates on:</p>
+                    <ul>
+                        <ListItem text='Product discovery and building what matters' />
+                        <ListItem text='Measuring to ensure updates are a success' />
+                        <ListItem text='And much more!' />
+                    </ul>
+                    <div className="inputs">
+                        <div className="messages">
+                            <p className="email">Email address</p>
+                            <p className="error none">Valid email required</p>
+                        </div>
 
-                    <input className="input-email" type='email' placeholder="email@company.com"></input>
-                    <input className="input-submit" type='submit' value='Subscribe to monthly newsletter' onClick={validation}></input>
+                        <input className="input-email" type='email' placeholder="email@company.com"></input>
+                        <input className="input-submit" type='submit' value='Subscribe to monthly newsletter' onClick={validation}></input>
+                    </div>
+                </div>
+                <div className="illustrattion">
+                    <img src={desktopImg} className="desktop" alt="illustration-sign-up"></img>
+                    <img src={mobileImg} className="mobile" alt="illustration-sign-up"></img>
                 </div>
             </div>
-            <div className="illustrattion">
-                <img src={desktopImg} className="desktop" alt="illustration-sign-up"></img>
-                <img src={mobileImg} className="mobile" alt="illustration-sign-up"></img>
+            <div className="success none">
+                <img src={success} alt="success"></img>
+                <h2 className="title">Thanks for subscribing!</h2>
+                <p className="description">A confirmation email has been sent to <span></span>.
+                    Please open it and click the button inside to confirm your subscription.</p>
+                <input className="input-submit" type='submit' value='Dismiss message'></input>
             </div>
-        </div>
+        </>
     )
 }
 
